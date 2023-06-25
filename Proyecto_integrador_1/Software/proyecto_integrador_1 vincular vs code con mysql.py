43f41431347a8edc5bff4cef5e7f577313697520
María Micaela Vilchez Cabrera <micaelavilchez@gmail.com>
2023-06-25 16:26:20 -0300

    Update proyecto_integrador_1 vincular vs code con mysql.py

--- Proyecto_integrador_1/Software/proyecto_integrador_1 vincular vs code con mysql.py
+++ Proyecto_integrador_1/Software/proyecto_integrador_1 vincular vs code con mysql.py
@@ -3,8 +3,31 @@ import mysql.connector
 midb = mysql.connector.connect(
  host="localhost",
  user="root",
- password="Noah2016",
- database="Proyecto_integrador_1"
+ password="Prueba!123",
+ database="Proyectointegradorv01"
 )
 print(midb)
+
+def insertar_Producto(self, iD, modelo, numero_serie, direccion_instalacion, coordenadas, estado):
+    if self.conexion.is_connected():
+        try:
+            cursor = self.conexion.cursor()
+            senteciaSQL="INSERT INTO dispositivos_iot values('01_Luz','Phillips_Hue','L00001', 'Cerro Colorado 1458','(51° 30’ 30’’ N; 0° 7’ 32’’ O)','En proceso')"
+            data = (dispositivos_iot.getiD(),
+                    dispositivos_iot.getmodelo(),
+                    dispositivos_iot.numero_serie(),
+                    dispositivos_iot.direccion_instalacion(),
+                    dispositivos_iot.coordenadas(),
+                    dispositivos_iot.getestado(),
+                    )
+            
+            cursor.execute(sentenciaSQL,data)
+            cursor.Conexion.commit()
+            cursor.Conexion.close()   
+            print("Producto insertado correctamente")
+
+        except mysql.connector.Error as descripcionDelError:
+            print("¡Hubo un error al intentar conctar la Base de Datos", descripcionDelError)
+
+
 
